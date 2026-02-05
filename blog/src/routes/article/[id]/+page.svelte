0ebca67dc80b7e6dfe4e61f1762dbbde82331f3b
Dalton Baker <dalton.s.baker@gmail.com>
2026-02-05 16:27:17 -0700

    Trying to make articles preload

--- blog/src/routes/article/[id]/+page.svelte
+++ blog/src/routes/article/[id]/+page.svelte
@@ -1,39 +1,11 @@
 <script>
-	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 	import { formatDate } from '$lib';
 	import { renderMarkdown } from '$lib/markdown';
 
-	let article = null;
-	let error = '';
-	let loading = true;
+	export let data;
 
-	onMount(async () => {
-		const articleId = $page.params.id;
-		const baseUrl = 'https://truth-data.dalt.dev/articles';
-
-		if (!articleId) {
-			error = 'No article specified.';
-			loading = false;
-			return;
-		}
-
-		try {
-			const res = await fetch(`${baseUrl}/${articleId}.json`);
-			if (!res.ok) {
-				error = 'Failed to load article.';
-			} else {
-				article = await res.json();
-				article.imageSrc = `https://truth-data.dalt.dev/images/${articleId}.webp`;
-				if (article.lastUpdated) article.imageSrc += `?v=${article.lastUpdated}`;
-			}
-		} catch (err) {
-			console.error(err);
-			error = 'Failed to load article.';
-		} finally {
-			loading = false;
-		}
-	});
+	$: article = data.article;
+	$: error = data.error;
 </script>
 
 <svelte:head>
@@ -43,15 +15,9 @@
 </svelte:head>
 
 <div class="d-flex justify-content-center w-100">
-    {#if loading}
-        <div class="text-center py-5">
-            <div class="spinner-border text-info" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    {:else if error}
+    {#if error}
         <div class="p-5 text-danger text-center">{error}</div>
-    {:else}
+    {:else if article}
         <div class="card article bg-secondary text-light border-light shadow-sm">
             <img src="{article.imageSrc}" class="card-img-top article" alt="{article.title} image">
             <div class="card-img-overlay">
