808db62c5a6039d0943bda4fa9c1c06dbddfcf23
Dalton Baker <dalton.s.baker@gmail.com>
2026-02-05 15:48:59 -0700

    Fixing post paths

--- forum/src/routes/+page.svelte
+++ forum/src/routes/+page.svelte
@@ -137,7 +137,7 @@
 		<div class="d-flex flex-column gap-2">
 			{#each posts as post, i}
 				<div>
-					<a href="/forum/post?id={post.id}" class="text-decoration-none text-light">
+					<a href="/post?id={post.id}" class="text-decoration-none text-light">
 						<div class="py-3 px-2 border-start border-3 post-hover">
 							<h5 class="fw-semibold mb-1">{post.title}</h5>
 							<small class="text-secondary d-flex flex-wrap gap-1">
